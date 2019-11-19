82d7d4a32f2aa067f9822d4b0d09c0ae66cb00c3
Alima Basheyeva <57907623+basheyevaa@users.noreply.github.com>
2019-11-19 18:07:35 +0600

    45.cpp

---
+++ 45.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x,y,z;
+    cin>>x>>y>>z;
+    if(x>10 && y>10 && z>10 && x%3==0 && y%3==0){
+        cout<<"yes";
+    }
+    else {
+        cout<<"no";
+    }
+    return 0;
+}
