033a701ffe0ba25e25baf6c1940aba9ba3fb901e
Alima Basheyeva <57907623+basheyevaa@users.noreply.github.com>
2019-11-19 18:02:52 +0600

    34.cpp

---
+++ 34.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cin>>x;
+    if(x>3) {
+        x+=10;
+    }
+    else {
+        x-=10;
+    }
+    cout<<x;
+    return 0;
+}
