12dec4183e26494f969b1e4ce3796b2482082b9e
Alima Basheyeva <57907623+basheyevaa@users.noreply.github.com>
2019-11-19 17:59:20 +0600

    29.cpp

---
+++ 29.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    cout<<b<<" "<<a;
+    return 0;
+}
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    swap(a,b);
+    cout<<a<<" "<<b;
+    return 0;
+}
