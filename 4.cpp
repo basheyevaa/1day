12ea58f42bb7377a0cbf3616d47cfac19c93a4c1
Alima Basheyeva <57907623+basheyevaa@users.noreply.github.com>
2019-11-19 17:41:52 +0600

    4.cpp

---
+++ 4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string a[100][100];
+    for(int i=0; i<5; i++){
+        for(int j=0; j<8; j++) {
+            cout<<"A";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
