f84c8590af3c57325ff2b0897655dff8b2441efa
Alima Basheyeva <57907623+basheyevaa@users.noreply.github.com>
2019-11-19 18:05:53 +0600

    41.cpp

---
+++ 41.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cin>>x;
+    if(x>100||x<-110){
+        cout<<0;
+    }
+    else {
+     cout<<x+1;
+    }
+    return 0;
+}
