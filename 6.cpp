5e1494395aaa0900af13133d6ca7e447385c2438
Alima Basheyeva <57907623+basheyevaa@users.noreply.github.com>
2019-11-19 17:44:59 +0600

    6.cpp

---
+++ 6.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+        int a=1+2-4;
+        cout<<a;
+    return 0;
+}
+
+
+ИЛИ 2-ОЙ ВАРИАНТ =)
+
+#include <iostream>
+using namespace std;
+int main()
+{
+        int a=1, b=2, c=4;
+        int d=a+b-c;
+        cout<<d;
+    return 0;
+}
