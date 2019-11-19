206237906f17c348601895ad918eed298947019d
Alima Basheyeva <57907623+basheyevaa@users.noreply.github.com>
2019-11-19 18:03:48 +0600

    36.cpp

---
+++ 36.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int m;
+    cin>>m;
+    if(m==1){
+        cout<<"January";
+    }
+    else if(m==2){
+        cout<<"Febr";
+    }
+    else if(m==3){
+        cout<<"March";
+    }
+    else if(m==4){
+        cout<<"Apr";
+    }
+    else if(m==5){
+        cout<<"May";
+    }
+    else if(m==6){
+        cout<<"June";
+    }
+    else if(m==7){
+        cout<<"July";
+    }
+    else if(m==8){
+        cout<<"AUg";
+    }
+    else if(m==9){
+        cout<<"Sept";
+    }
+    else if(m==10){
+        cout<<"Oct";
+    }
+    else if(m==11){
+        cout<<"Nov";
+    }
+    else if(m==12){
+        cout<<"Dec";
+    }
+    else {
+        cout<<"not exists";
+    }
+    return 0;
+}
