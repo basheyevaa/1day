c5e15c11adb8077cfc2f3dbb0ed7b205850f4914
Alima Basheyeva <57907623+basheyevaa@users.noreply.github.com>
2019-11-19 17:55:29 +0600

    23.cpp

---
+++ 23.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float x,a,y,k;
+    cin>>x,a,y,k;
+    cout<<(y*a)/x<<endl;
+    cout<<(k*x)/a<<"kg mojno kupit"<<endl;
+    return 0;
+}
