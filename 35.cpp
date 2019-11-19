1432980161cc9a39a0edd5d312a974b693ed0a38
Alima Basheyeva <57907623+basheyevaa@users.noreply.github.com>
2019-11-19 18:03:20 +0600

    35.cpp

---
+++ 35.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cin>>x;
+    if(x<7) {
+        cout<<"Yes";
+    }
+    else if(x>10) {
+        cout<<"No";
+    }
+    else if(x==9){
+        cout<<"ERror";
+    }
+    return 0;
+}
